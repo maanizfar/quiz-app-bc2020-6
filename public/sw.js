69ce25c1523daad28a10a83daa3c73a8a0f45217
Salman Zafar <maanizfar@gmail.com>
2020-08-06 17:58:54 +0500

    fixes cache files

--- public/sw.js
+++ public/sw.js
@@ -6,58 +6,55 @@ this.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(cacheData).then((cache) => {
       cache.addAll([
-        "/",
-        "index.html",
-        "/manifest.json",
-        "/favicon.ico",
-        "/logo192.png",
-        "/images/computer.png",
-        "/images/generalknowledge.png",
-        "/images/geography.png",
-        "/images/history.png",
-        "/images/music.png",
-        "/images/science.png",
-        "https://fonts.googleapis.com/icon?family=Material+Icons",
-        "/static/js/2.953c1251.chunk.js",
-        "/static/js/main.817d7748.chunk.js",
-
-        "https://opentdb.com/api.php?amount=10&type=multiple&category=9&difficulty=easy",
-        "https://opentdb.com/api.php?amount=10&type=multiple&category=9&difficulty=medium",
-        "https://opentdb.com/api.php?amount=10&type=multiple&category=9&difficulty=hard",
-
-        "https://opentdb.com/api.php?amount=10&type=multiple&category=12&difficulty=easy",
-        "https://opentdb.com/api.php?amount=10&type=multiple&category=12&difficulty=medium",
-        "https://opentdb.com/api.php?amount=10&type=multiple&category=12&difficulty=hard",
-
-        "https://opentdb.com/api.php?amount=10&type=multiple&category=17&difficulty=easy",
-        "https://opentdb.com/api.php?amount=10&type=multiple&category=17&difficulty=medium",
-        "https://opentdb.com/api.php?amount=10&type=multiple&category=17&difficulty=hard",
-
-        "https://opentdb.com/api.php?amount=10&type=multiple&category=18&difficulty=easy",
-        "https://opentdb.com/api.php?amount=10&type=multiple&category=18&difficulty=medium",
-        "https://opentdb.com/api.php?amount=10&type=multiple&category=18&difficulty=hard",
-
-        "https://opentdb.com/api.php?amount=10&type=multiple&category=22&difficulty=easy",
-        "https://opentdb.com/api.php?amount=10&type=multiple&category=22&difficulty=medium",
-        "https://opentdb.com/api.php?amount=10&type=multiple&category=22&difficulty=hard",
-
-        "https://opentdb.com/api.php?amount=10&type=multiple&category=23&difficulty=easy",
-        "https://opentdb.com/api.php?amount=10&type=multiple&category=23&difficulty=medium",
-        "https://opentdb.com/api.php?amount=10&type=multiple&category=23&difficulty=hard",
+        // "/",
+        // "index.html",
+        // "/manifest.json",
+        // "/favicon.ico",
+        // "/logo192.png",
+        // "/images/computer.png",
+        // "/images/generalknowledge.png",
+        // "/images/geography.png",
+        // "/images/history.png",
+        // "/images/music.png",
+        // "/images/science.png",
+        // "https://fonts.googleapis.com/icon?family=Material+Icons",
+        // "https://opentdb.com/api.php?amount=10&type=multiple&category=9&difficulty=easy",
+        // "https://opentdb.com/api.php?amount=10&type=multiple&category=9&difficulty=medium",
+        // "https://opentdb.com/api.php?amount=10&type=multiple&category=9&difficulty=hard",
+        // "https://opentdb.com/api.php?amount=10&type=multiple&category=12&difficulty=easy",
+        // "https://opentdb.com/api.php?amount=10&type=multiple&category=12&difficulty=medium",
+        // "https://opentdb.com/api.php?amount=10&type=multiple&category=12&difficulty=hard",
+        // "https://opentdb.com/api.php?amount=10&type=multiple&category=17&difficulty=easy",
+        // "https://opentdb.com/api.php?amount=10&type=multiple&category=17&difficulty=medium",
+        // "https://opentdb.com/api.php?amount=10&type=multiple&category=17&difficulty=hard",
+        // "https://opentdb.com/api.php?amount=10&type=multiple&category=18&difficulty=easy",
+        // "https://opentdb.com/api.php?amount=10&type=multiple&category=18&difficulty=medium",
+        // "https://opentdb.com/api.php?amount=10&type=multiple&category=18&difficulty=hard",
+        // "https://opentdb.com/api.php?amount=10&type=multiple&category=22&difficulty=easy",
+        // "https://opentdb.com/api.php?amount=10&type=multiple&category=22&difficulty=medium",
+        // "https://opentdb.com/api.php?amount=10&type=multiple&category=22&difficulty=hard",
+        // "https://opentdb.com/api.php?amount=10&type=multiple&category=23&difficulty=easy",
+        // "https://opentdb.com/api.php?amount=10&type=multiple&category=23&difficulty=medium",
+        // "https://opentdb.com/api.php?amount=10&type=multiple&category=23&difficulty=hard",
+        // "/static/js/2.953c1251.chunk.js",
+        // "/static/js/main.1e1311be.chunk.js",
       ]);
     })
   );
 });
 
-this.addEventListener("fetch", (event) => {
+this.addEventListener("fetch", function (event) {
   event.respondWith(
-    caches
-      .match(event.request, { cacheName: cacheData, ignoreVary: true })
-      .then(function (response) {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    caches.open(cacheData).then(function (cache) {
+      return cache.match(event.request).then(function (response) {
+        return (
+          response ||
+          fetch(event.request).then(function (response) {
+            cache.put(event.request, response.clone());
+            return response;
+          })
+        );
+      });
+    })
   );
 });
